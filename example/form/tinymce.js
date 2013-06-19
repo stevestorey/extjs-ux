6ea75e16f8ebd2af2eecde4c423b3b09d1d2a508
Steve Storey <sstorey@gmail.com>
2013-06-20 00:13:47 +0100

    Update TinyMCE form examples

--- example/form/tinymce.js
+++ example/form/tinymce.js
@@ -79,14 +79,11 @@ Ext.onReady(function() {
                 name: 'bio',
                 fieldLabel: 'Biography',
                 labelAlign: 'top',
-                //height: 200,
+                height: 300,
                 anchor: '100%',
                 //disabled: true,
                 tinymceConfig: {
-                    theme_advanced_buttons1: 'fullscreen,|,undo,redo,|,bold,italic,strikethrough,|,charmap,|,removeformat,code',
-                    theme_advanced_buttons2: '',
-                    theme_advanced_buttons3: '',
-                    theme_advanced_buttons4: ''
+                    toolbar1: 'fullscreen,|,undo,redo,|,bold,italic,strikethrough,|,charmap,|,removeformat,code',
                 }
             }
         ],
